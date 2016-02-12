bdcc105087fc9f02da62a969428e784936e65da4
Sergio Sanabria <sergioj.sanabria@gmail.com>
2016-02-12 11:56:25 -0300

    commit para RC 1 :-)

--- www/js/SiMOR.js
+++ www/js/SiMOR.js
@@ -24,11 +24,389 @@ $$(document).on('ajaxComplete', function () {
     myApp.hideIndicator();
 });
 
+
+
+
+myApp.onPageInit('about', function (page) {
+//    $$(page.container).find('.content-block').append('')
+    $$.get("terminos.html", function (data) {
+        $$(page.container).find('.content-block').html(data);
+    });
+});
+
+
+/* ===== Change statusbar bg when panel opened/closed ===== */
+$$('.panel-left').on('open', function () {
+    $$('.statusbar-overlay').addClass('with-panel-left');
+});
+//$$('.panel-right').on('open', function () {
+//    $$('.statusbar-overlay').addClass('with-panel-right');
+//});
+$$('.panel-left, .panel-right').on('close', function () {
+    $$('.statusbar-overlay').removeClass('with-panel-left with-panel-right');
+});
+
+
+colorAzul = "#0082FF";
+colorVerde = "#009688";
+colorAmarillo = "#FFC870";
+colorRojo = "#FF5A5E";
+colorActual = "#000";
+colorGris = "#EEEEEE";
+var myDoughnutChart = false;
+
+var data = new Array ();
+    
+
+
+//var color = {
+//    value: 1,
+//    color: colorGris,
+//    highlight: colorGris
+//};
+//
+//data.push(color);
+//$$("#grafico").html('');
+//
+//    var ctx = document.getElementById("grafico").getContext("2d");
+//
+//
+//        if (myDoughnutChart)
+//            myDoughnutChart.destroy();
+//    
+//     myDoughnutChart = new Chart(ctx).Doughnut(data, {
+//        responsive: false,
+//        tooltipTemplate: "<%if (label){%><%=label%> <%}%>",
+//        percentageInnerCutout: 65,
+//        segmentStrokeColor: "rgba(255, 255, 255, 0)",
+//         animation : false, 
+//        
+//
+//    });
+
+
+function buscarRioByPuerto(puerto) {
+    var retorno = 'rio';
+    $$.each(datos, function (i, v) {
+        if (v.puerto == puerto) {
+            retorno = v.rio;
+        }
+    });
+
+    return retorno;
+}
+
+function buscarMedidaByPuerto(puerto) {
+    var retorno = 'rio';
+    $$.each(datos, function (i, v) {
+        if (v.puerto == puerto) {
+            retorno = v;
+        }
+    });
+
+    var valorAlerta = parseFloat(retorno.alerta.replace(',', '.'));
+    var valorEvacuacion = parseFloat(retorno.evacuacion.replace(',', '.'));
+
+    var valorVerde = valorAlerta;
+    var valorAmarillo = valorEvacuacion - valorAlerta;
+    var valorRojo = (valorEvacuacion * 20) / 100;
+    var valorActual = (valorEvacuacion * 1.5) / 100;
+    var valor = parseFloat(retorno.ultimo_registro.replace(',', '.'));
+
+
+    var data = Array();
+
+
+    var textoColor = '';
+    var textoValor = '';
+    var textoDescripcion = '';
+
+
+    var total = valorEvacuacion * 1.15;
+
+    var porcentajeValor = (valor * 100) / total;
+    var porcentajeValorAlerta = (valorAlerta * 100) / total;
+    var porcentajeValorEvacuacion = (valorEvacuacion * 100) / total;
+
+
+    //verde
+    if (valor < valorAlerta) {
+
+        textoColor = colorAzul;
+
+        var val1 = valorVerde - valor;
+        var color = {
+            value: porcentajeValor,
+            color: colorAzul,
+            highlight: colorAzul,
+             label: "Estado actual seguro a " + valor + " metros",
+        };
+
+        data.push(color);
+
+        var color = {
+            value: porcentajeValorAlerta - 1 - porcentajeValor,
+            color: colorGris,
+            highlight: colorGris,
+        };
+
+        data.push(color);
+
+        var color = {
+            value: 1,
+            color: colorAmarillo,
+            highlight: colorAmarillo,
+            label: "Estado alerta a partir de los " + valorAlerta + " metros"
+        };
+
+        data.push(color);
+
+        var color = {
+            value: porcentajeValorEvacuacion - 1 - porcentajeValorAlerta,
+            color: colorGris,
+            highlight: colorGris,
+        };
+
+        data.push(color);
+
+        var color = {
+            value: 1,
+            color: colorRojo,
+            highlight: colorRojo,
+            label: "Estado de evacuación a partir de los " + valorEvacuacion + " metros"
+        };
+
+        data.push(color);
+
+
+        var color = {
+            value: 100 - porcentajeValorEvacuacion,
+            color: colorGris,
+            highlight: colorGris,
+        };
+
+        data.push(color);
+
+
+    }
+
+    //amarillo
+    if (valor >= valorAlerta && valor < valorEvacuacion) {
+
+        textoColor = colorAmarillo;
+
+        var color = {
+            value: porcentajeValorAlerta - 1,
+            color: colorAzul,
+            highlight: colorAzul,
+        };
+
+        data.push(color);
+
+
+        var color = {
+            value: 1,
+            color: colorAmarillo,
+            highlight: colorAmarillo,
+            label: "Estado de alerta a partir de los " + valorAlerta + " metros"
+        };
+
+        data.push(color);
+
+        var color = {
+            value: porcentajeValor - porcentajeValorAlerta,
+            color: colorAzul,
+            highlight: colorAzul,
+            label: "Estado actual de alerta a " + valor + " metros",
+        };
+
+        data.push(color);
+
+
+        var color = {
+            value: porcentajeValorEvacuacion - porcentajeValor - 1,
+            color: colorGris,
+            highlight: colorGris,
+            
+        };
+
+        data.push(color);
+
+        var color = {
+            value: 1,
+            color: colorRojo,
+            highlight: colorRojo,
+            label: "Estado de evacuación a partir de los " + valorEvacuacion + " metros"
+        };
+
+
+
+        data.push(color);
+
+
+        var color = {
+            value: 100 - porcentajeValorEvacuacion,
+            color: colorGris,
+            highlight: colorGris,
+           
+           
+        };
+
+        data.push(color);
+
+
+    }
+
+
+    //rojo
+    if (valor >= valorEvacuacion) {
+
+        textoColor = colorRojo;
+
+        var color = {
+            value: porcentajeValorAlerta - 1,
+            color: colorAzul,
+            highlight: colorAzul
+        };
+
+        data.push(color);
+
+
+        var color = {
+            value: 1,
+            color: colorAmarillo,
+            highlight: colorAmarillo,
+            label: "Estado de alerta a partir de los " + valorAlerta + " metros"
+        };
+
+        data.push(color);
+
+        var color = {
+            value: porcentajeValorEvacuacion - porcentajeValorAlerta - 1,
+            color: colorAzul,
+            highlight: colorAzul
+            
+        };
+
+        data.push(color);
+
+
+
+        var color = {
+            value: 1,
+            color: colorRojo,
+            highlight: colorRojo,
+            label: "Estado de evacuación a partir de los " + valorEvacuacion + " metros"
+        };
+
+
+        data.push(color);
+        
+        var color = {
+            value: porcentajeValor - porcentajeValorEvacuacion ,
+            color: colorAzul,
+            label: "Estado actual de evacuación a " + valor + " metros"
+            
+        };
+
+        data.push(color);
+
+
+        var color = {
+            value: 100 - porcentajeValor,
+            color: colorGris,
+            highlight: colorGris
+            
+        };
+
+        data.push(color);
+
+
+    }
+
+
+
+    $$("#grafico").removeClass('hidden');
+    $$("#img-circulo").css('display','none');
+    $$("#grafico").html('');
+
+    var ctx = document.getElementById("grafico").getContext("2d");
+
+
+        if (myDoughnutChart)
+            myDoughnutChart.destroy();
+    
+     myDoughnutChart = new Chart(ctx).Doughnut(data, {
+        responsive: false,
+        tooltipTemplate: "<%if (label){%><%=label%> <%}%>",
+        percentageInnerCutout: 65,
+        segmentStrokeColor: "rgba(255, 255, 255, 0)",
+         //animation : false, 
+        
+
+    });
+
+
+
+    textoValor = valor + ' metros';
+    textoDescripcion = controlNivel(retorno.variacion);
+
+    textoColor = controlNivelColor(retorno.variacion);
+    
+    $$("#valor-rio").html(textoValor);
+    $$("#descripcion-rio").html(textoDescripcion).css("color", textoColor);
+    $$("#valor-alerta").html(valorAlerta+ ' mt <br> alerta');
+    $$("#valor-evacuacion").html(valorEvacuacion+ ' mt <br> evacuac.');
+
+    return retorno;
+}
+
+function controlNivel(variacion) {
+    variacion = parseFloat(variacion.replace(',', '.'));
+
+    var texto = '';
+    if (variacion == 0) {
+        texto = '0 mts =';
+    }
+
+    if (variacion > 0) {
+        texto = variacion + ' mts ▲';
+    }
+
+    if (variacion < 0) {
+        texto = variacion + ' mts ▼';
+    }
+
+    return texto;
+}
+
+
+function controlNivelColor (variacion) {
+    variacion = parseFloat(variacion.replace(',', '.'));
+
+    var color = '#FFF';
+   
+    if (variacion > 0) {
+        color = colorRojo;
+    }
+
+    if (variacion < 0) {
+        color = colorVerde;
+    }
+    
+    if (variacion == 0) {
+        color = colorAzul;
+    }
+
+    return color;
+}
+
 //cuando inicializa la app
 var ubicacion = new Array();
 var datos = new Array();
 $$.ajax({
-    url: 'http://179.43.125.144/simor_web_service/index.php/welcome/get_niveles',
+    url: 'http://179.43.125.144/simor_web_service/api/niveles.json',
+    //url: 'http://localhost/SiMOR-backend/web/app.php/api/niveles.json',
     dataType: 'json',
     async: false,
     success: function (data) {
@@ -36,12 +414,12 @@ $$.ajax({
 //            console.log(dato);
             if (dato.evacuacion != '-' && dato.puerto != 'undefined') {
                 datos.push(dato);
-                
+
                 ubicacion.push(dato.puerto);
             }
 
         });
-        
+
         ubicacion.sort();
     }
 });
@@ -66,276 +444,3 @@ var pickerUbicacion = myApp.picker({
         buscarMedidaByPuerto(picker.value);
     }
 });
-
-
-
-myApp.onPageInit('about', function (page) {
-//    $$(page.container).find('.content-block').append('')
-    $$.get("terminos.html", function (data) {
-        $$(page.container).find('.content-block').html(data);
-    });
-});
-
-
-/* ===== Change statusbar bg when panel opened/closed ===== */
-$$('.panel-left').on('open', function () {
-    $$('.statusbar-overlay').addClass('with-panel-left');
-});
-//$$('.panel-right').on('open', function () {
-//    $$('.statusbar-overlay').addClass('with-panel-right');
-//});
-$$('.panel-left, .panel-right').on('close', function () {
-    $$('.statusbar-overlay').removeClass('with-panel-left with-panel-right');
-});
-
-
-colorVerde = "#5AD3D1";
-colorAmarillo = "#FFC870";
-colorRojo = "#FF5A5E";
-colorActual = "#000";
-
-
-function buscarRioByPuerto(puerto) {
-    var retorno = 'rio';
-    $$.each(datos, function (i, v) {
-        if (v.puerto == puerto) {
-            retorno = v.rio;
-        }
-    });
-
-    return retorno;
-}
-
-function buscarMedidaByPuerto(puerto) {
-    var retorno = 'rio';
-    $$.each(datos, function (i, v) {
-        if (v.puerto == puerto) {
-            retorno = v;
-        }
-    });
-    
-    var valorAlerta = parseFloat(retorno.alerta.replace(',', '.'));
-    var valorEvacuacion = parseFloat(retorno.evacuacion.replace(',', '.'));
-
-    var valorVerde = valorAlerta;
-    var valorAmarillo = valorEvacuacion - valorAlerta;
-    var valorRojo = (valorEvacuacion * 20) / 100;
-    var valorActual = (valorEvacuacion * 1.5) / 100;
-    var valor = parseFloat(retorno ['ult. registro'].replace(',', '.'));
-    
-    
-    var data = Array();
-    
-    
-    var textoColor = '';
-    var textoValor = '';
-    var textoDescripcion = '';
-
-
-    //verde
-    if (valor < valorAlerta) {
-        
-        textoColor = colorVerde;
-        
-        var val1 = valorVerde - valor;
-        var color = {
-            value: valor,
-            color: colorVerde,
-            highlight: colorVerde,
-            label: "Estado seguro de 0 a " + retorno.alerta+ " metros"
-        };
-        
-        data.push(color);
-        
-        var color = {
-            value: valorActual,
-            color: colorActual,
-            highlight: colorActual,
-            label: "Estado actual seguro a " + valor+ " metros"
-        };
-        
-        data.push(color);
-        
-       var color = {
-            value: val1,
-            color: colorVerde,
-            highlight: colorVerde,
-            label: "Estado seguro de 0 a " + retorno.alerta+ " metros"
-        };
-        
-        data.push(color);
-        
-        
-    }else{
-        var color = {
-            value: valorVerde,
-            color: colorVerde,
-            highlight: colorVerde,
-            label: "Estado seguro de 0 a " + retorno.alerta+ " metros"
-        };
-        
-        data.push(color);
-    }
-
-
-    //amarillo
-    if (valor >= valorAlerta && valor < valorEvacuacion) {
-        textoColor = colorAmarillo;
-        
-        var val1 = valorEvacuacion - valor;
-        var val2 = valor - valorAlerta;
-        var color = {
-            value: val2,
-            color: colorAmarillo,
-            highlight: colorAmarillo,
-            label: "Estado alerta " + retorno.alerta + " a " + retorno.evacuacion+ " metros"
-        };
-        
-        data.push(color);
-        
-        var color = {
-            value: valorActual,
-            color: colorActual,
-            highlight: colorActual,
-            label: "Estado actual de alerta a " +valor+ " metros"
-        };
-        
-        data.push(color);
-        
-       var color = {
-            value: val1,
-            color: colorAmarillo,
-            highlight: colorAmarillo,
-            label: "Estado alerta " + retorno.alerta + " a " + retorno.evacuacion+ " metros"
-        };
-        
-        data.push(color);
-    }else{
-        
-         var color ={
-            value: valorAmarillo,
-            highlight: colorAmarillo,
-            color: colorAmarillo,
-            label: "Estado alerta " + retorno.alerta + " a " + retorno.evacuacion+ " metros"
-        };
-        data.push(color);
-    }
-
-    //rojo
-    if (valor >= valorEvacuacion) {
-        textoColor = colorRojo;
-        var val1 = valorEvacuacion + valorRojo  - valor;
-        var val2 = valor  - valorEvacuacion;
-        
-        var color = {
-            value: val2,
-            color: colorRojo,
-            highlight: colorRojo,
-            label: "Estado evacuación de 0 a " + retorno.alerta+ " metros"
-        };
-        
-        data.push(color);
-        
-        var color = {
-            value: valorActual,
-            color: colorActual,
-            highlight: colorActual,
-            label: "Estado actual evacuación " + valor+ " metros"
-        };
-        
-        data.push(color);
-        
-       var color = {
-            value: val1,
-            color: colorRojo,
-            highlight: colorRojo,
-            label: "Estado evacuación de 0 a " + retorno.alerta+ " metros"
-        };
-        
-        data.push(color);
-    }else{
-        var color = {
-            value: valorRojo,
-            highlight: colorRojo,
-            color: colorRojo,
-            label: "Estado evacuación de " + retorno.evacuacion + " metros ",
-            
-        };
-        data.push(color);
-    }
-
-
-
-//
-//    var data = [
-//        {
-//            value: valorVerde,
-//            color: "#5AD3D1",
-//            highlight: "#5AD3D1",
-//            label: "Estado seguro de 0 a " + retorno.alerta+ " metros"
-//        },
-//        {
-//            value: valorAmarillo,
-//            highlight: "#FFC870",
-//            color: "#FFC870",
-//            label: "Estado alerta " + retorno.alerta + " a " + retorno.evacuacion+ " metros"
-//        },
-//        {
-//            value: valorRojo,
-//            highlight: "#FF5A5E",
-//            color: "#FF5A5E",
-//            label: "Estado evacuación de " + retorno.evacuacion + " metros "
-//        }
-//
-//    ];
-
-
-
-
-    var ctx = document.getElementById("grafico").getContext("2d");
-    
-   
-
-    var myDoughnutChart = new Chart(ctx).Doughnut(data, {
-        responsive: true,
-        tooltipTemplate: "<%if (label){%><%=label%> <%}%>",
-        
-    });
-    
-    
-
-
-    
-
-
-    
-
-
-
-    textoValor = valor + ' metros';
-    textoDescripcion = controlNivel(retorno.variacion);
-
-    $$("#valor-rio").html(textoValor).css("color", textoColor);
-    $$("#descripcion-rio").html(textoDescripcion);
-
-    return retorno;
-}
-
-function controlNivel(variacion) {
-    variacion = parseFloat(variacion.replace(',', '.'));
-
-    var texto = '';
-    if (variacion == 0) {
-        texto = '0 mts =';
-    }
-
-    if (variacion > 0) {
-        texto = variacion + ' mts ↑';
-    }
-
-    if (variacion < 0) {
-        texto = variacion + ' mts ↓';
-    }
-
-    return texto;
-}
